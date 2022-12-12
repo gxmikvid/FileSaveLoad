e5dcd26bfa121a7ba0acb394fe95c6a76f5ec68c
gxmikvid <69439560+gxmikvid@users.noreply.github.com>
2022-12-12 09:36:49 +0100

    v1.0

---
+++ File/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace NevDatumFeladat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameTXTBX = new System.Windows.Forms.TextBox();
+            this.Save = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.Load = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 31);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(536, 234);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Név";
+            // 
+            // NameTXTBX
+            // 
+            this.NameTXTBX.Location = new System.Drawing.Point(53, 6);
+            this.NameTXTBX.Name = "NameTXTBX";
+            this.NameTXTBX.Size = new System.Drawing.Size(100, 20);
+            this.NameTXTBX.TabIndex = 2;
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(159, 4);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Mentés";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(240, 4);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 4;
+            this.Load.Text = "Töltés";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(321, 6);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 265);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.NameTXTBX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NameTXTBX;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ File/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace NevDatumFeladat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(NameTXTBX.Text))
+            {
+                MessageBox.Show("nincs név!");
+                return;
+            }
+            if (string.IsNullOrEmpty(richTextBox1.Text))
+            {
+                MessageBox.Show("nincs szöveg!");
+                return;
+            }
+
+            saveFileDialog1.Filter = "Szöveg file| *.txt|Vesszővel tagolt szöveg file | *.csv|Minden file| *.*";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string szoveg = string.Join(";", NameTXTBX.Text, dateTimePicker1.Text, richTextBox1.Text);
+                string filename = saveFileDialog1.FileName;
+                File.WriteAllText(filename, szoveg);
+            }
+            else
+            {
+                MessageBox.Show("Megszakadt a folyamat!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string filename = openFileDialog1.FileName;
+                string beolvasottSzoveg = File.ReadAllText(filename);
+                string[] adat = beolvasottSzoveg.Split(';');
+                NameTXTBX.Text = adat[0];
+                dateTimePicker1.Text = adat[1];
+                richTextBox1.Text = adat[2];
+            }
+        }
+    }
+}
